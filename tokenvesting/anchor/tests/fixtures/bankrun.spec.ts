fd082a5a9eea8c5950d021115f3241007a160acf
Abhilov23 <abhilovgupta01@gmail.com>
2025-06-29 18:12:38 +0530

    setting up bankrun.spec.ts file

---
+++ tokenvesting/anchor/tests/fixtures/bankrun.spec.ts
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Keypair, PublicKey } from "@solana/web3.js";
+import * as anchor from "@coral-xyz/anchor";
+import {BanksClient, ProgramTestContext, startAnchor} from "solana-bankrun";
+import IDL from "../../target/idl/vesting.json";
+import { SYSTEM_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/native/system";
+import { BankrunProvider } from "anchor-bankrun";
+import { Program } from "@coral-xyz/anchor";
+import { Vesting } from "anchor/target/types/vesting";
+
+describe("Vesting smart contract tests", ()=>{
+ let beneficiary: Keypair;
+ let context: ProgramTestContext;
+ let provider: BankrunProvider;
+ let program: Program<Vesting>;
+ let banksClient : BanksClient;
+ let employer: Keypair;
+
+
+
+    beforeAll(async () => {
+    beneficiary = new anchor.web3.Keypair();  
+    
+    context = await startAnchor(
+        "",
+        [{name:"vesting",  programId: new PublicKey(IDL.address)}
+        ],[
+            {
+                address: beneficiary.publicKey,
+                info: {
+                    lamports: 1000_000_000,
+                    data: Buffer.alloc(0),
+                    owner: SYSTEM_PROGRAM_ID,
+                    executable: false
+                }
+            }
+        ]
+    );
+
+    provider = new BankrunProvider(context);
+
+    anchor.setProvider(provider);
+
+    program = new Program<Vesting>(IDL as Vesting, provider);
+
+    banksClient = context.banksClient;
+
+    employer = provider.wallet.payer;
+  
+})
+})
